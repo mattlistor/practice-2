7063f811af22369e50a3b2a45722db98c00c2ad2
mattlistor <mattlistor@gmail.com>
2021-11-30 00:54:53 -0500

    new updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+function countInitial() {
+  console.log('hello')
+  return 4
+}
 
 function App() {
+
+  const [count, setCount] =  useState(countInitial())
+  //first in array is current state, second in array is function that updates current state
+
+  function decrementCount(){
+    setCount(prevCount => prevCount + 1)
+  }
+
+  function incrementCount(){
+    setCount(prevCount => prevCount + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+
     </div>
   );
 }
